10a89bfb8feb3d50431070653d8b99eff499d304
fsichetti <f.sichetti@gmail.com>
2022-12-02 17:25:43 +0100

    shaders dual face

--- shaders/smoothF.glsl
+++ shaders/smoothF.glsl
@@ -3,14 +3,17 @@
 layout(location = 1) uniform vec3 lightDir;
 layout(location = 2) uniform float alpha;
 const float minLight = 0.2;
+const vec3 frontColour = vec3(0.8, 0.8, 1.0);
+const vec3 backColour = vec3(1.0, 0.8, 0.8);
 
 layout(location = 0) in vec3 iNormal;
-layout(location = 1) in vec3 iColour;
 
 layout(location = 0) out vec4 oColour;
 
 
 void main() {
-    float light = max(dot(iNormal, lightDir), minLight);
-    oColour = vec4(iColour * light, alpha);
+    const float d = dot(iNormal, lightDir); // Replace this with view dir!!
+    const vec3 faceColour = d > 0 ? frontColour : backColour;
+    float light = max(abs(d), minLight);
+    oColour = vec4(faceColour * light, alpha);
 }
